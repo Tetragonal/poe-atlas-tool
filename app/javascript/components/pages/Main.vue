4ff4980d7ae7c81a51d1b168a5e3f8f6b044e952
Kevin Tang <kevin_m_tang@yahoo.com>
2018-07-30 00:15:48 -0700

    Add Main.vue

---
+++ app/javascript/components/pages/Main.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    (Main) Hello World!
+    <b-alert show>Default Alert</b-alert>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Main"
+  }
+</script>
+
+<style scoped>
+
+</style>
