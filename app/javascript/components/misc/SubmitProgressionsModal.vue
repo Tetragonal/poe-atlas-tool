908b1a24f1f874e99d8b2271daada18551d4d148
Kevin Tang <kevin_m_tang@yahoo.com>
2018-08-22 10:12:12 -0700

    Add demo image for AtlasParser

--- app/javascript/components/misc/SubmitProgressionsModal.vue
+++ app/javascript/components/misc/SubmitProgressionsModal.vue
@@ -4,14 +4,19 @@
            title="Confirm Submit"
            @ok="$emit('ok')"
            ref="loginModal">
-    <p>
-      asdasdasd
-    </p>
+    <b-form-checkbox v-model="status">
+      Set me to online for 12 hours (Trade tool)
+    </b-form-checkbox>
   </b-modal>
 </template>
 
 <script>
   export default {
-    name: "SubmitProgressionsModal"
+    name: "SubmitProgressionsModal",
+    data() {
+      return {
+        status: undefined
+      }
+    }
   }
 </script>
